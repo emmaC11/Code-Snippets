9b50aa553dc52fb3f26a5854993fea24ffcdec04
Emma C <c.emma2112@gmail.com>
2022-11-20 18:55:04 +0000

    template literals JS

--- JavaScript/template-literals.js
+++ JavaScript/template-literals.js
@@ -1,4 +1,5 @@
 //syntax for template literals in JS
 
 let name = "emma"
-console.log(`hello my name is ${name}`)
+console.log(`hello my name is ${user.name}`)
+console.log(`hello my name is {name}`)
